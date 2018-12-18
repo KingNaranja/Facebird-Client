eb2d96f52ac7767e1521dab728878faa52560446
Natesuri <natesuri@gmail.com>
2018-12-18 14:44:09 -0500

    Update and Index post request
    From event.js to ajax call, user can update and get all posts (from all
    users). Not test yet, pending completion of client forms.
    
    Collaborators: Eric and Nate

--- assets/scripts/post/post-ui.js
+++ assets/scripts/post/post-ui.js
@@ -10,7 +10,32 @@ const createPostFailure = function (apiData) {
   console.log(`you didn't create a post!`)
 }
 
+const getAllPostsSuccess = apiData => {
+  console.log(apiData)
+  console.log('you got posts!')
+}
+
+const getAllPostsFailure = apiData => {
+  console.log(apiData)
+  console.log(`you didn't get posts`)
+}
+
+const updatePostSuccess = apiData => {
+  console.log(apiData)
+  console.log('you updated a post!')
+}
+
+const updatePostFailure = apiData => {
+  console.log(apiData)
+  console.log(`you didn't update a post!`)
+}
+
 module.exports = {
   createPostSuccess,
-  createPostFailure
+  createPostFailure,
+  getAllPostsSuccess,
+  getAllPostsFailure,
+  updatePostSuccess,
+  updatePostFailure
+
 }
