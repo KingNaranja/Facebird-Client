fff8b3deb6469db7e030fe6fef50c4852c9fb360
KingNaranja <kledaa3@gmail.com>
2018-12-20 13:41:32 -0500

    removed unnecessary comments

--- assets/scripts/toastr/toasts.js
+++ assets/scripts/toastr/toasts.js
@@ -21,27 +21,6 @@ const uiToasts = {
 
 
 
-// var i = 0;
-
-// $('#tryMe').click(function () {
-//     $('#tryMe').prop('disabled', true);
-//     delayToasts();
-// });
-
-// function delayToasts() {
-//     if (i === toasts.length) { return; }
-//     var delay = i === 0 ? 0 : 2100;
-//     window.setTimeout(function () { showToast(); }, delay);
-
-//     // re-enable the button        
-//     if (i === toasts.length-1) {
-//         window.setTimeout(function () {
-//             $('#tryMe').prop('disabled', false);
-//             i = 0;
-//         }, delay + 1000);
-//     }
-// }
-
 function showToast(action) {
     toastr.options.positionClass = 'toast-top-full-width';
     toastr.options.extendedTimeOut = 0; //1000;
